9d4e880f823f9134933b2aa6ae00f1b02e0c4ac7
clarababette <clara_coetzee@zoho.com>
2021-11-25 12:05:20 +0200

    Update cars.js

--- cars.js
+++ cars.js
@@ -8,19 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
     var carsTemplate = Handlebars.compile(carsTemplateSource);
     var brandsElem = document.querySelector('.brands');
 
-    axios.get('http://api-tutor.herokuapp.com/v1/colors')
+    axios.get('https://api-tutor.herokuapp.com/v1/colors')
         .then((response) => {
             colorsElem.innerHTML = listTemplate(
                 { items: response.data }
             );
         });
-    axios.get('http://api-tutor.herokuapp.com/v1/cars')
+    axios.get('https://api-tutor.herokuapp.com/v1/cars')
         .then((response) => {
             carsElem.innerHTML = carsTemplate(
                 { cars: response.data }
             );
         });
-    axios.get('http://api-tutor.herokuapp.com/v1/makes')
+    axios.get('https://api-tutor.herokuapp.com/v1/makes')
         .then((response) => {
             brandsElem.innerHTML = listTemplate(
                 { items: response.data }
